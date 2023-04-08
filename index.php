ed57d9ab32bdf777d03fb1c21193a333408fc58a
TOUSSAINT YANN <yann.toussaint2@etu.unistra.fr>
2023-04-08 11:25:42 +0200

    Commit initial

---
+++ index.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Projet Prog-Web2 Yann Toussaint 2023</title>
+    </head>
+    <body>
+        <p>Is this working?</p>
+        <?php print("yolo") ?>
+    </body>
+</html>
